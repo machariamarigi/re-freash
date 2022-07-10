67072ce519b784a37c79c2425fae5d9dd06f0ddc
machariamarigi <machariamarigi@gmail.com>
2022-07-10 23:35:02 +0300

    AddTodo component whind handles the creation of todos

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ITodo, TodoContextType } from "../@types/todo";
+import { TodoContext } from "../context/todoContext";
+
+const AddTodo: React.FC = () => {
+    const { saveTodo } = React.useContext(TodoContext) as TodoContextType;
+    const [ formData, setFormData ] = React.useState<ITodo | {}>()
+
+    const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
+        setFormData({
+            ...formData,
+            [e.currentTarget.id]: e.currentTarget.value,
+        })
+    }
+
+    const handleSaveTodo = (e: React.FormEvent, formData: ITodo | any) => {
+        e.preventDefault();
+        saveTodo(formData);
+    }
+    return (
+        <form className="form" onSubmit={(e) => handleSaveTodo(e, formData)}>
+            <div>
+                <div>
+                    <label htmlFor="name">Title</label>
+                    <input type="text" id="description" onChange={handleForm}/>
+                </div>
+            </div>
+
+            <button disabled={formData === undefined ? true : false}>Add Todo</button>
+        </form>
+    )
+}
+
+export default AddTodo;
