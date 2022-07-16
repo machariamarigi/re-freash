a07e35f662c84910325f3b5536449136dfb3f799
machariamarigi <machariamarigi@gmail.com>
2022-07-16 22:31:59 +0300

    link to the rehook page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC, ReactElement } from "react";
 import { NavLink } from "react-router-dom";
+import './HomePage.css';
 
 const HomePage: FC<any> = (): ReactElement => {
     return (
-        <div className="home-page">
+        <div className="homepage">
             Contains these modules:
             <ul>
                 <li><NavLink to="/todos">Todos</NavLink></li>
+                <li><NavLink to="/rehook">Hooks</NavLink></li>
             </ul>
         </div>
     )
