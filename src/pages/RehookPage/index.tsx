66e9a22be5771e85d90de979e5087cb187e289e5
machariamarigi <machariamarigi@gmail.com>
2022-07-17 14:13:57 +0300

    add UseEffectComponent

--- src/pages/RehookPage/index.tsx
+++ src/pages/RehookPage/index.tsx
@@ -1,10 +1,13 @@
 import React, { FC, ReactElement } from 'react';
+import UseEffectComponent from '../../components/UseEffectComponent';
 import UseStateComponent from '../../components/UseStateComponent';
 import './RehookPage.css';
 
 const RehookPage: FC<any> = (): ReactElement => {
     return (
         <div>
+            <h1>useEffect</h1>
+            <UseEffectComponent />
             <h1>useState</h1>
             <UseStateComponent />
         </div>
