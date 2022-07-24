b52a81842580606f4a650ffef96ba3de3f1d51ce
machariamarigi <machariamarigi@gmail.com>
2022-07-24 18:06:21 +0300

    use useMemo to only show portland beer logos

--- src/components/CustomHookComponent.tsx
+++ src/components/CustomHookComponent.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Beverage } from '../@types/beverage';
 import useFetchData from "../hooks/useFetchData"
 
 const CustomHookComponent = () => {
-    const { data, done } = useFetchData<Beverage[]>("/data/hv-taplist.json");
+    const { data } = useFetchData<Beverage[]>("/data/hv-taplist.json");
+    const portlandTaps = useMemo(() => 
+        (data || []).filter(beverage => beverage.producerLocation.includes("Portland")),
+        [data]
+    )
+
+    
+
     return(
         <div>
-            { done && (
-                <img src={data![0].logo} alt="Beverage logo" />
+            { portlandTaps.length && (
+                <img src={portlandTaps![0].logo} alt="Beverage logo" />
             )}
         </div>
 
